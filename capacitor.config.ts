c1ce50d97c73349b178345fe7ca92c6b91aa43c5
Gabriel Lucas Arruda <gabriellucas_arruda@hotmail.com>
2022-04-17 15:20:33 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Upx-5Sem',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
